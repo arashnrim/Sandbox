31d81d6e555352ae875cf9af67d89178c4fc4d91
Arash <47273556+arashnrim@users.noreply.github.com>
2020-11-03 13:00:52 +0800

    ✨ Add support for arguments
    This commit adds support for the arguments --location (to specify a
    location before program execution) and --norepeat. I think this is
    enough for a v1.1.0!

--- pyweather/main.py
+++ pyweather/main.py
@@ -1,5 +1,6 @@
 import configparser
 import os
+import argparse
 from time import sleep
 
 from art import tprint
@@ -10,6 +11,10 @@ from styling import Color
 from parse import parse
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
+AP_PARSER = argparse.ArgumentParser()
+AP_PARSER.add_argument("--l", "--location", help="get weather information for a custom location", dest="location")
+AP_PARSER.add_argument("--nr", "--norepeat", help="do not prompt for re-entry", action="store_true", dest="norepeat")
+AP_ARGS = AP_PARSER.parse_args()
 
 # Tries to read if the user has set up the program before.
 # Otherwise, the user is directed to onboarding.
@@ -30,29 +35,42 @@ if not configured:
     sleep(2)
     welcome()
 
-sleep(0.5)
 use_default_location = True
 repeat = "y"
 while repeat == "y" or repeat == "Y":
-    if use_default_location:
-        default_location = retrieve_default_location()
-        if default_location != "":
-            data = retrieve_data(default_location)
+    if not AP_ARGS.location:
+        if use_default_location:
+            default_location = retrieve_default_location()
+            if default_location != "":
+                data = retrieve_data(default_location)
+        else:
+            location = input("Enter a location: ")
+            data = retrieve_data(location)
     else:
-        location = input("Enter a location: ")
-        data = retrieve_data(location)
+        data = retrieve_data(AP_ARGS.location)
 
     # noinspection PyUnboundLocalVariable
     if data is not None:
         country_code = data["sys"]["country"]
 
         # noinspection PyUnboundLocalVariable,PyUnboundLocalVariable
-        print("=== {}Weather for {}, {}{} ===".format(Color.BOLD, default_location if use_default_location else location,
-                                                      country_code, Color.END))
+        if not AP_ARGS.location:
+            print("=== {}Weather for {}, {}{} ===".format(Color.BOLD,
+                                                          default_location if use_default_location else location,
+                                                          country_code, Color.END))
+        else:
+            print("=== {}Weather for {}, {}{} ===".format(Color.BOLD,
+                                                          AP_ARGS.location,
+                                                          country_code, Color.END))
         parse(data)
-        sleep(4)
+
+    if AP_ARGS.norepeat:
+        break
+
+    sleep(4)
     repeat = input("\nEnter another location? (y/n): ")
     if repeat == "y" or "Y":
         use_default_location = False
+        AP_ARGS.location = ""
     else:
         break
