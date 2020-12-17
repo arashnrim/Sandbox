9e47cc221ede6afe8401f3a8f281e323e2e4f1b4
Arash <47273556+arashnrim@users.noreply.github.com>
2020-12-17 11:34:39 +0800

    ✨ Implement parsing

--- devprojects/pyscraper/scraper.py
+++ devprojects/pyscraper/scraper.py
@@ -1,6 +1,9 @@
 import sys
+
 import requests
 from bs4 import BeautifulSoup
+
+from format import formats
 
 try:
     LINK = sys.argv[1]
@@ -10,10 +13,33 @@ except IndexError:
 REQUEST = requests.get(LINK)
 TEXT = REQUEST.text
 
-if "cnn.com" in LINK:
-    ELEMENT = "span"
-else:
-    ELEMENT = "p"
+for link in formats:
+    if link in LINK:
+        BODY_ELEMENT = formats[link]["body_element"]
+        BODY_CLASS = formats[link]["body_class"]
+        headline_class = formats[link]["headline"]
+        author_element = formats[link]["author_element"]
+        author_class = formats[link]["author_class"]
+        date_element = formats[link]["date_element"]
+        date_class = formats[link]["date_class"]
 
-for paragraph in BeautifulSoup(TEXT, features="html.parser").find_all(ELEMENT):
-    print(paragraph.get_text())
+        # Gets the headline of the article.
+        headline = BeautifulSoup(TEXT, features="html.parser").find_all("h1", {"class": headline_class})[0].get_text()
+        print("\033[1m{}\033[0m".format(headline))
+
+        # Gets the author of the article.
+        author = BeautifulSoup(TEXT, features="html.parser").find_all(author_element, {"class": author_class})[
+            0].get_text()
+        print(author)
+
+        # Gets the date of the article.
+        if date_element and date_class:
+            date = BeautifulSoup(TEXT, features="html.parser").find_all(date_element, {"class": date_class})[
+                0].get_text()
+            print(date)
+
+print()
+
+# Gets the main content of the article.
+for element in BeautifulSoup(TEXT, features="html.parser").find_all(BODY_ELEMENT, {"class": BODY_CLASS}):
+    print(element.get_text())
